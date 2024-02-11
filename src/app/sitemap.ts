1d17370e6327a49b9512e09951e673a8cc929815
LucasAEscudero <lucasescudero5629@gmail.com>
2024-02-11 16:55:19 -0300

    sitemap reafactor route

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/galery`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/galery/[date]`,
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/favorites`,
