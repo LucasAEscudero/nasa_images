82a09e323ddeddc81fe097723883e46562d62b77
LucasAEscudero <lucasescudero5629@gmail.com>
2024-03-12 23:33:45 -0300

    refactor footer text

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="mt-2 mx-3 py-1 border-t-[1px]">
       <small className="mb-2 text-xs block text-center">
-        &copy; 2024 Lucas. Todos los derechos reservados.
+        &copy; 2024 Lucas. All rights reserved.
       </small>
       <p className="text-xs text-center">
         <span className="font-semibold">About this web site:</span> devloped
